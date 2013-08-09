3c1ae6b15e26047cb0970e8b10ac2f43856f00b4
Aleksander Cichowicz <olek@simple-systems.pl>
2013-08-10 00:42:39 +0200

    added sql file, changes in gitignore

--- public/cadmin.php
+++ public/cadmin.php
@@ -6,17 +6,9 @@ include '../libs/Smarty/Smarty.class.php';
 $cadmin = new Cadmin;
 $smarty = new Smarty;
 
-//switch ($_GET) {
-//    case 'check':
-//        echo 'sd';
-//        $cadmin->check;
-//        break;
-//}
 if ($_GET['check'] != null) {
     $smarty->configLoad('../configs/config.conf', 'Database');
     echo $smarty->getVariable('host');
-    
-    //echo DB;
     $cadmin->check();
 }
 ?>
